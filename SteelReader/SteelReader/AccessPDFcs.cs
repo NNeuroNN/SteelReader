391ab5f718461e42e9d8535ef67a4d993bf6d05e
NNeuroNN <08neuron08@gmail.com>
2018-04-26 12:51:43 +0300

    Opening

--- SteelReader/SteelReader/AccessPDFcs.cs
+++ SteelReader/SteelReader/AccessPDFcs.cs
@@ -9,33 +9,45 @@ namespace SteelReader
     static class AccessPDFcs
     {
 
+        
+
         static public PdfReader GetPdf(string path) {
             return new PdfReader(path);
         }
+
         static public List<PdfDictionary> Pages(this PdfReader pdf)
         {
             List<PdfDictionary> dict = new List<PdfDictionary>(); ;
-            for (int i = 0; i < pdf.NumberOfPages; i++) {
+            for (int i = 1; i < pdf.NumberOfPages; i++) {
                 dict.Add(pdf.GetPageN(i));
             }
             return dict;
         }
+
         static public List<PdfDictionary> GetAnnots(this List<PdfDictionary> dicts) {
             List<PdfDictionary> list = new List<PdfDictionary>();
             foreach (var dict in dicts)
             {
                 PdfArray annotAray = dict.GetAsArray(PdfName.ANNOTS);
                 for (int i = 0; i < annotAray.Length; i++) {
-                    
-                    list.Add(annotAray.GetAsDict(i));
+                    try
+                    {
+                        list.Add(annotAray.GetAsDict(i));
+                    }
+                    catch (Exception) { }
                 }
             }
          
             return list;
         }
-        static public PdfString GetAnnotItem(this PdfDictionary dict, PdfName name) {
 
-            return dict.GetAsString(name);
+        static public PdfString GetAnnotItem(this PdfDictionary dict, PdfName name) {
+            try
+            {
+                return dict.GetAsString(name);
+            }
+            catch (Exception) { }
+            return new PdfString("Неудача");
         }
     }
 }
